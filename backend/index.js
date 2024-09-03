6a76b9b86a3b18f6dee573f1451c57e70fa081e4
zaky ramadhan <zakyramadhanxjobs@gmail.com>
2024-09-03 15:31:10 +0700

    update: change name

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import dbConnect from "./config/dbConnect.js";
+import connectDB from "./config/dbConnect.js";
 
 dotenv.config();
 
-dbConnect();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
-app.use();
-
-app.use();
-
-app.use();
-
-app.use();
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
