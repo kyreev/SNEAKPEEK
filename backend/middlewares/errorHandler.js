0529c46719aadd3c1470865c5dc822048e29d7b3
zaky ramadhan <zakyramadhanxjobs@gmail.com>
2024-09-11 13:24:13 +0700

    create: add error handler

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,22 @@
+// Middleware to handle routes not found (404)
+export const notFound = (req, res, next) => {
+  const error = new Error(`Not Found: ${req.originalUrl}`);
+  res.status(404).json({
+    success: false,
+    message: error.message,
+  });
+  next(error);
+};
+
+// Middleware to handle application errors
+export const errorHandler = (err, req, res, next) => {
+  console.error(`Error: ${err.message}`);
+
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode).json({
+    success: false,
+    message: err.message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : null, // Only show stack trace in development
+  });
+};
