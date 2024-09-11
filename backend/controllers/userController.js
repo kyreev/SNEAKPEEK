871b04befbcf045b9386c568982807c82b4ea3ba
zaky ramadhan <zakyramadhanxjobs@gmail.com>
2024-09-11 14:55:28 +0700

    update: add login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../config/jwtToken.js";
 
 export const createUser = asyncHandler(async (req, res) => {
   const { email } = req.body;
@@ -8,12 +9,12 @@ export const createUser = asyncHandler(async (req, res) => {
   if (existingUser) {
     return res.status(400).json({
       success: false,
-      message: "User already exist",
+      message: "User already exists",
     });
   }
 
   const newUser = await User.create(req.body);
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     message: "User created successfully",
     user: {
@@ -23,6 +24,40 @@ export const createUser = asyncHandler(async (req, res) => {
       email: newUser.email,
       mobile: newUser.mobile,
       role: newUser.role,
+    },
+  });
+});
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  const isMatch = await user.isPasswordMatched(password);
+  if (!isMatch) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid credentials",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Login successful",
+    user: {
+      _id: user._id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      mobile: user.mobile,
+      role: user.role,
+      token: generateToken(user._id),
     },
   });
 });
