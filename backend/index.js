8e01c674bf23367a9e931e5a85ec02bac59e0511
zaky ramadhan <zakyramadhanxjobs@gmail.com>
2024-09-11 13:22:01 +0700

    update: import authRouter

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/dbConnect.js";
+import authRouter from "./routes/authRoute.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use("/api/user", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
