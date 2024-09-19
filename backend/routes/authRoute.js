747743ac21c61f115bee2dd36a49dd19c4b2f8a7
zaky ramadhan <zakyramadhanxjobs@gmail.com>
2024-09-19 14:38:05 +0700

    update: add new router

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -5,6 +5,7 @@ import {
   getAllUsers,
   getUserById,
   deleteUser,
+  updateUser,
 } from "../controllers/userController.js";
 
 const router = express.Router();
@@ -15,5 +16,6 @@ router.post("/login", loginUser);
 router.get("/", getAllUsers);
 router.get("/:id", getUserById);
 router.delete("/:id", deleteUser);
+router.put("/:id", updateUser);
 
 export default router;
